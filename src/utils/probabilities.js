934f697233e5173c17c892214266a0f99a0571ff
KienNguyenz <kien03.bx@gmail.com>
2026-02-18 21:10:49 +0700

    new

--- src/utils/probabilities.js
+++ src/utils/probabilities.js
@@ -18,8 +18,8 @@ export const getWeightedReward = (rewards) => {
 };
 
 export const defaultRewards = [
-    { id: 1, label: "100,000₫", value: 100000, weight: 25, color: "#FFEB3B" },
-    { id: 2, label: "200,000₫", value: 200000, weight: 25, color: "#D32F2F" },
-    { id: 3, label: "500,000₫", value: 500000, weight: 25, color: "#D32F2F" },
-    { id: 4, label: "1,000,000₫", value: 1000000, weight: 25, color: "#D32F2F" },
+    { id: 1, label: "100,000₫", value: 100000, weight: 30, color: "#FFEB3B" },
+    { id: 2, label: "200,000₫", value: 200000, weight: 30, color: "#D32F2F" },
+    { id: 3, label: "500,000₫", value: 500000, weight: 20, color: "#D32F2F" },
+    { id: 4, label: "1,000,000₫", value: 1000000, weight: 20, color: "#D32F2F" },
 ];
